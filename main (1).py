6beb68e9c1079cdc1f01f16019e0c1cefab6660c
ValBalagulan <vnbalagulan2030@crc.pshs.edu.ph>
2026-01-08 16:35:37 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+
+def CentimeterstoInches():
+    Centimeter=float(input("Enter centimeters"))
+    Inches=Centimeter/2.54
+    print(Centimeter,"cm to Inches is",Inches)
+    
+CentimeterstoInches()
